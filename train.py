a3abd4b651c7fd13a615b869ea4699a316e20da5
James Cameron <jamesadcameron111@gmail.com>
2020-03-31 19:17:44 -0300

    add functionality to choose window of data

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
                         help='number of columns in dataset that are not part of the emg signal', type=int, default=3)
     parser.add_argument('--sample_length', type=int, help='number of samples per channel', default=200)
     parser.add_argument('--num_of_channels', help='number of channels', type=int, default=8)
+    parser.add_argument('--sample_offset', help='discrete starting sample point for window', type=int, default=0)
     parser.add_argument('--channel_skip_count', help='fraction of channels to use', type=int, default=1)
     parser.add_argument('--sample_freq', help='number of channels', type=int, default=1000)
     parser.add_argument('--spectral_type', help='number of channels', default='stft')
@@ -25,32 +26,45 @@ if __name__ == '__main__':
     # SinGAN parameters
     parser.add_argument('--mode', help='set generation mode', default='train')
 
+    # init params
     opt = parser.parse_args()
     opt = functions.post_config(opt)
     Gs = []
     Zs = []
     reals = []
     NoiseAmp = []
-    dir2save = functions.generate_dir2save(opt)
 
+    # create directory
+    dir2save = functions.generate_dir2save(opt)
     if (os.path.exists(dir2save)):
-        print('trained model already exist')
+        print('Trained model already exists! Stopping...')
+        exit(1)
     else:
         try:
             os.makedirs(dir2save)
         except OSError:
             pass
 
-        opt.nc_im = opt.num_of_channels//opt.channel_skip_count
-        opt.nc_z = opt.num_of_channels//opt.channel_skip_count
-        x = get_multi_channel_spectral_array(opt)
+    # generate spectral image
+    opt.nc_im = opt.num_of_channels // opt.channel_skip_count
+    opt.nc_z = opt.num_of_channels // opt.channel_skip_count
+    x = get_multi_channel_spectral_array(opt)
 
-        real = functions.np2torch(x, opt)
+    # select sample window
+    if x.shape[1] >= opt.sample_offset + opt.sample_length:
+        x = x[:, opt.sample_offset:opt.sample_offset + opt.sample_length, :]
+    else:
+        print("Sample length is greater then supplied data! Stopping...")
+        exit(1)
 
-        functions.adjust_scales2image(real, opt)
-        if opt.mode == 'animation_train':
-            opt.min_size = 20
-            train_from_signal(opt, Gs, Zs, reals, NoiseAmp, real)
-        else:  # opt.mode == train
-            train_from_signal(opt, Gs, Zs, reals, NoiseAmp, real)
+    # convert to tensor
+    real = functions.np2torch(x, opt)
+
+    # train
+    functions.adjust_scales2image(real, opt)
+    if opt.mode == 'animation_train':
+        opt.min_size = 20
+        train_from_signal(opt, Gs, Zs, reals, NoiseAmp, real)
+    else:  # opt.mode == train
+        train_from_signal(opt, Gs, Zs, reals, NoiseAmp, real)
 
